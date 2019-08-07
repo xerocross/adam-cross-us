24c63851d8e8670340f04aa547161cfe9a8cc224
Adam <noneya@busi.ness>
2019-08-07 14:00:36 -0400

    remove disclaimer

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import headerText from '../images/header-text.svg'; // with import
 import TopSites from "./TopSites.jsx";
-import Disclaimer from "./Disclaimer.jsx";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Profile from "./Profile.jsx";
@@ -14,9 +13,7 @@ export default class MainLayout extends React.Component {
     render () {
         return (
             <div className="App">
-                <Disclaimer />
                 <div className = "container">
-                    
                     <header className="App-header">
                         <h1><img className = "headerTextImg" alt = "Adam Cross Professional Portfolio" src = {headerText} /></h1>
                     </header>
