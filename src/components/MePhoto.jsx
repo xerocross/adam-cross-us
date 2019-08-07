8b492366833e0cfea60181f5a3411bf5dd69a717
Adam <noneya@busi.ness>
2019-08-07 07:25:47 -0400

    cleanup and move config to const config file

--- src/components/MePhoto.jsx
+++ src/components/MePhoto.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import mePhoto from '../images/me_photo2.png';
 import "./MePhoto.scss";
+import Config from "../config.js";
+const introVidConfig = Config.introVidConfig;
 export default function () {
     return (
         <div className = "MePhoto">
-            <iframe className = "introVid" title = "intro-vid" width="560" height="315" src="https://www.youtube.com/embed/96G7DvK7mNk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe className = "introVid" title = "intro-vid" width="560" height="315" src={introVidConfig.src} frameBorder="0" allow={introVidConfig.allow} allowFullScreen></iframe>
         </div>
     )
 }
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    introVidConfig : {
+        src : "https://www.youtube.com/embed/96G7DvK7mNk",
+        allow : "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    },
+}
