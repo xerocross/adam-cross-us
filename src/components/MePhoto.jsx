e296de18bf47f0281cc6e761877dfaedbc8e2a08
Adam <noneya@busi.ness>
2019-08-07 04:09:56 -0400

    replace static photo with intro video

--- src/components/MePhoto.jsx
+++ src/components/MePhoto.jsx
@@ -4,7 +4,7 @@ import "./MePhoto.scss";
 export default function () {
     return (
         <div className = "MePhoto">
-            <img src={mePhoto} width = "1600" height = "1600" alt = "a face pic of me, Adam Cross"/>
+            <iframe className = "introVid" title = "intro-vid" width="560" height="315" src="https://www.youtube.com/embed/96G7DvK7mNk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
