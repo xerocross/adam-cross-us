756baa20b7db771692f582eda555d10cde3b9806
Adam <noneya@busi.ness>
2019-08-09 22:19:47 -0400

    change to static photo

--- src/components/MePhoto.jsx
+++ src/components/MePhoto.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./MePhoto.scss";
-import Config from "../config.js";
-const introVidConfig = Config.introVidConfig;
+// import Config from "../config.js";
+import myPng from "../images/me_photo2.png";
+// const introVidConfig = Config.introVidConfig;
+
 export default function () {
     return (
         <div className = "MePhoto">
-            <iframe className = "introVid" title = "intro-vid" width="560" height="315" src={introVidConfig.src} frameBorder="0" allow={introVidConfig.allow} allowFullScreen></iframe>
+            <img alt = "Adam Cross" src = {myPng} />
+            {/* <iframe className = "introVid" title = "intro-vid" width="560" height="315" src={introVidConfig.src} frameBorder="0" allow={introVidConfig.allow} allowFullScreen></iframe> */}
         </div>
     )
 }
